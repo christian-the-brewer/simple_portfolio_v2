7386fef70f16ca77595c8fef7b273074dfbbfa0a
christian-the-brewer <brewer.christiang@gmail.com>
2023-02-04 10:12:15 -0500

    Adds routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/shared/Header';
+import Footer from './components/shared/Footer';
+import Home from './components/Home';
+import Experience from './components/Experience';
+import IndexProjects from './components/projects/IndexProjects';
+import ShowProject from './components/projects/ShowProject';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Header />
+        <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<IndexProjects />} />
+        <Route path='/project/:id' element={<ShowProject />} />
+        <Route path='/experience' element={<Experience />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/projects/IndexProjects.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IndexProjects = () => {
+  return (
+    <div>IndexProjects</div>
+  )
+}
+
+export default IndexProjects
