902e9cf5110f77b726977338528bbe65735205f0
christian-the-brewer <brewer.christiang@gmail.com>
2023-02-04 10:05:00 -0500

    setting up project

---
+++ src/projectList.js
@@ -0,0 +1,43 @@
+import Test_Image1 from "./assets/test_image1.jpeg"
+import Goblin_Image from "./assets/goblin_story.png"
+import Gpt3_Image from "./assets/gpt3.png"
+import Tictactoe_Image from "./assets/tictactoe.png"
+
+export const projectList = [
+    {
+        name: "Streamgenie",
+        image: Test_Image1,
+        description: "A MERN app with user auth to track movies and shows across streaming psatforms and add to a watch list.",
+        skills: "Javascript, HTML, CSS, React, Bootstrap, MongoDB, Express, Node.",
+        url: "https://github.com/christian-the-brewer/streamgenie-react-front",
+        githubUrl: "https://github.com/christian-the-brewer/streamgenie-react-front",
+
+    },
+    {
+        name: "GPT-3",
+        image: Gpt3_Image,
+        description: "A fake site for OpenAI's GPT-3 nueral network.",
+        skills: "Javascript, HTML, CSS",
+        url: "https://christian-the-brewer.github.io/gpt3_site/",
+        githubUrl: "https://github.com/christian-the-brewer/gpt3_site",
+
+    },
+    {
+        name: "Goblin Story",
+        image: Goblin_Image,
+        description: "My first full web app and game. Written during my second week of coding bootcamp. Using only HTML and javascript, I wanted to make a fun game with my partner, who created all the artwork. I am keeping it in its original unfactored state to remember how far I have come so far in my coding journey. ",
+        skills: "Javascript, HTML, CSS",
+        url: "https://christian-the-brewer.github.io/Goblin_Story/",
+        githubUrl: "https://github.com/christian-the-brewer/Goblin_Story",
+
+    },
+    {
+        name: "Tic Tac Toe",
+        image: Tictactoe_Image,
+        description: "A two-player browser-based version of the game.",
+        skills: "Javascript, HTML, CSS",
+        url: "https://christian-the-brewer.github.io/tic_tac_toe/",
+        githubUrl: "https://github.com/christian-the-brewer/tic_tac_toe",
+
+    },
+]
